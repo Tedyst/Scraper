2c3cb0a7d17d1df8bd5b6eab1c42f9af84871f65
Tedy Stoica <stoicatedy@gmail.com>
2018-09-19 20:57:20 +0300

    more useful

--- scrape.py
+++ scrape.py
@@ -1,55 +1,59 @@
 from bs4 import BeautifulSoup
 from urllib2 import Request, urlopen
 import decimal
+from datetime import datetime
 import time
-import mysql.connector
 import schedule
-mydb = mysql.connector.connect(
-  host="mysql",
-  user="tedy",
-  passwd="cosica",
-  database="tedy"
-)
-mycursor = mydb.cursor()
+
+f = open("emag.txt", "a+")
+read = open("read.txt", "r")
+
 
 def findLinks(url, base):
-	userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
-	req = Request(url, None, {'User-Agent': userAgent})
-	html = urlopen(req).read()	
-	soup = BeautifulSoup(html, "lxml")
-	if "emag" in url:
-		for link in soup.find_all('div',attrs={'class' : 'card-section-wrapper'}):
-			if base == "ryzen":
-				nume = str(link.contents[1].contents[1].contents[0].contents[1].contents[0].get('alt').split("+", 1)[0].replace(u'\xa0', u' ').split(",", 1)[0].split("Procesor AMD ", 1)[1])
-			if base == "memorie":
-				nume = str(link.contents[1].contents[1].contents[0].contents[1].contents[0].get('alt').split("+", 1)[0].replace(u'\xa0', u' ').split("Memorie ", 1)[1])
-			pret = int(link.contents[5].contents[2].contents[2].contents[0].replace(".",""))
-			timp = int(int(time.time()))
-			data = (timp, nume, pret)
-			print data
-			if base == "ryzen":
-				mycursor.execute("INSERT INTO ryzen (timp, nume, pret) VALUES (%s, %s, %s)", data)
-			if base == "memorie":
-				mycursor.execute("INSERT INTO memorie (timp, nume, pret) VALUES (%s, %s, %s)", data)
-			# print(link.contents[5].contents[2].contents[2].contents[0].replace(".","") + " " + link.contents[1].contents[1].contents[0].contents[1].contents[0].get('alt').split("+", 1)[0])
-	mydb.commit()
+    userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
+    req = Request(url, None, {'User-Agent': userAgent})
+    html = urlopen(req).read()
+    soup = BeautifulSoup(html, "lxml")
+    if "emag" in url:
+        for link in soup.find_all('div', attrs={'class': 'card-section-wrapper'}):
+            if base == "ryzen":
+                nume = str(link.contents[1].contents[1].contents[0].contents[1].contents[0].get('alt').split(
+                    "+", 1)[0].replace(u'\xa0', u' ').split(",", 1)[0].split("Procesor AMD ", 1)[1])
+            if base == "memorie":
+                nume = str(link.contents[1].contents[1].contents[0].contents[1].contents[0].get(
+                    'alt').split("+", 1)[0].replace(u'\xa0', u' ').split("Memorie ", 1)[1])
+            pret = int(
+                link.contents[5].contents[2].contents[2].contents[0].replace(".", ""))
+            timp = int(int(time.time()))
+            data = (timp, nume, pret)
+            print data
+
 
 def findPrice(url):
-	userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
-	req = Request(url, None, {'User-Agent': userAgent})
-	html = urlopen(req).read()	
-	soup = BeautifulSoup(html, "lxml")
-	if "emag" in url:
-		for link in soup.find_all('p',attrs={'class' : 'product-new-price'}):
-			return decimal.Decimal(link.contents[0].strip().replace(".","").replace(" ","").replace("Lei","").replace("<sup>",",").replace("</sup>","").replace("<span>","").replace("</span>",""))
-	if "altex" in url:
-		for link in soup.find_all('span',attrs={'class' : 'Price-int'}):
-			return decimal.Decimal(link.contents[0])
+    userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
+    req = Request(url, None, {'User-Agent': userAgent})
+    html = urlopen(req).read()
+    soup = BeautifulSoup(html, "lxml")
+    if "emag" in url:
+        for link in soup.find_all('p', attrs={'class': 'product-new-price'}):
+            return link.contents[0].strip().replace(".", "").replace(" ", "").replace("Lei", "").replace("<sup>", ",").replace("</sup>", "").replace("<span>", "").replace("</span>", "") + " eMAG "
+    if "altex" in url:
+        for link in soup.find_all('span', attrs={'class': 'Price-int'}):
+            return link.contents[0] + " Altex "
+
+
+def log(rezultat):
+    print rezultat
+    f.write("[" + datetime.now().strftime('%c')+"]    " + rezultat + "\n")
+
 
 def cauta():
-	findLinks("https://www.emag.ro/procesoare/filter/familie-procesor-f2666,amd-ryzen-7-v24725/familie-procesor-f2666,amd-ryzen-5-v24753/familie-procesor-f2666,amd-ryzen-3-v25688/c?ref=lst_leftbar_2666_25688,lst_leftbar_2666_24753,lst_leftbar_2666_24725", "ryzen")
-	findLinks("https://www.emag.ro/memorii/brand/corsair,hyperx,kingston/filter/tip-memorie-f5,ddr4-v10954/capacitate-f141,8-gb-v389/module-f142,single-channel-v7234/pret,intre-0-si-500/c", "memorie")
-	print time.time()
+    for line in read:
+        log(findPrice(line.split("|")[0]) +
+            line.split("|")[1].replace('\n', ''))
+    f.flush()
+    print "Citit perturile la " + datetime.now().strftime('%c')
+
 
 cauta()
 
